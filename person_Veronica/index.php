5031870b65265a1742c38b68b1a423c8866da09f
cartveroj <cartagenaveronica.19@gmail.com>
2023-09-21 16:57:43 +0200

    introduccion PHP

---
+++ person_Veronica/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nomAlumna="Veronica";
+    $cognomsAlumne="Cartagena";
+    $edat="29";
+    $dataDeNaixement;
+    $telèfon;
+    $adreçaPostal;
+    $adreçaElectrònica;
+    $treballa;
+    $alcada;
+
+    ?>
+    <div>
+        <h1> descripcion  <?php echo $nom." ".$apellido ?> </h1>
+    </div>
+    <div>
+        <h2> Les dades de <?php echo $nom ?> son:</h2>
+    </div>
+    <div>
+        <ul>
+            <li> Es diu <?php echo $nom ?></li>
+            <li>Te <?php $edat ?> </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</body>
+</html>
