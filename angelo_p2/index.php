81732e11e248b1ae4a2b97b5b3b70e6c2ecb7d43
angelusMZ19 <angelomontenegro200306@gmail.com>
2023-09-29 17:34:11 +0200

    ejercicio_angelo

---
+++ angelo_p2/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica2</title>
+</head>
+<body>
+    <?php
+        $mesa[];
+        for($i=0; i<10; i++){
+        
+            
+        }
+        $echo mesa[i];
+    ?>
+    <form action="">
+        <?php for($a=0; a<10;a++){?>
+                
+                <?php 
+                $comensal= rand(0,5);
+                $mesa[$i]= $comensal
+                if($mesa[i]==5){
+                    $mensaje = "La taula".$a."plena";
+                    echo $mensaje;
+                }else if($mesa[i]==0){
+                    $mensaje= "La taula".$a."es buida";
+                    echo $menaje;
+                } else{
+                    $mensaje= "La taul".$a."te".$mesa[$a]."comensals"; 
+                    echo $mensaje
+                }
+                ?>
+            <?php }?>
+
+    
+    </form>
+          
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
